349b601e272a9a5e8ab689d4c91e051b165ecf17
Marcelo Hage <marcelorhage@hotmail.com>
2023-02-05 17:15:34 -0300

    att string

---
+++ strings.js
@@ -0,0 +1,23 @@
+ // Sting - Funçoes:
+    
+ const escola = 'cod3r'
+    
+ console.log(escola.charAt(4))
+ // Pega a letra que esta atribuida ao indice escolhido.
+ 
+ console.log(escola.charAt(3))
+ // Vai imprimir o codigo HTML do caracter, pois e um numero.
+ 
+ console.log(escola.indexOf('3'))
+ // Mostra em que indice, na palavra, esta o digito escolhido.
+ 
+ console.log(escola.substring(1))
+ // Considera a palavra a partir do indice indicado (1).
+ 
+ console.log(escola.substring(0, 3))
+ // Considera a palavra a partir do indice indicado (1), ate antes do segundo indice indicado (3).
+ 
+ console.log('Escola '.concat(escola).concat("!"))
+ // Concatenar uma string em outra ou em um literal (dado que nao armazena em uma variavel).
+ 
+ console.log()
